7a1a66dc6e7b54f55b0a7bc6b65b24dcfe96677f
taizul <mdtaizulislam50@gmail.com>
2023-09-13 13:14:20 +0600

    just

--- app/src/main/java/com/example/toasty_dependancy/MainActivity.kt
+++ app/src/main/java/com/example/toasty_dependancy/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.toasty_dependancy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.toasty.SimpleToaster
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         SimpleToaster.showToast(applicationContext,"Hi")
+        Toast.makeText(this,"hello", Toast.LENGTH_LONG).show()
     }
 }
